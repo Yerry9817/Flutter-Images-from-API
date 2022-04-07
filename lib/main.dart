2252c3ef5421b53be4ca95e06fe5d0659c58f660
YERRY RODRIGUEZ BARQUERO <yerry.rodriguez.barquero@est.una.ac.cr>
2022-04-06 19:51:11 -0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MiApp());
+
+class MiApp extends StatelessWidget {
+  const MiApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "Mi app",
+      home: Inicio(),
+    );
+  }
+}
+
+class Inicio extends StatefulWidget {
+  const Inicio({Key? key}) : super(key: key);
+
+  @override
+  State<Inicio> createState() => _InicioState();
+}
+
+class _InicioState extends State<Inicio> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Hola Mundo"),
+      ),
+      body: Column(
+        //para agregar varias imagenes se necesita una columna
+        children: [
+          Container(
+            //por cada imagen, un container
+            padding: const EdgeInsets.all(
+                20.5), //para padding siempre en float values
+            child: Image.network(
+                "https://i0.wp.com/codigoespagueti.com/wp-content/uploads/2021/11/Naruto-fanart-imagina-villano-historia.jpg?fit=1280%2C720&quality=80&ssl=1"),
+          ),
+          Container(
+            padding: const EdgeInsets.all(
+                20.5), //para padding siempre en float values
+            child: Image.network(
+                "https://fotografias-neox.atresmedia.com/clipping/cmsimages01/2022/03/04/6F3E2E33-8AAF-4607-8B3E-80261F00FB86/naruto_98.jpg?crop=1920,1080,x0,y0&width=1900&height=1069&optimize=high&format=webply"),
+          ),
+        ],
+      ),
+    );
+  }
+}
